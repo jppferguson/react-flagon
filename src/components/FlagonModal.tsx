20b754b446c537c1a8d65253dfe626d1ed98417a
Jamie Ferguson <git@jppferguson.com>
2020-02-01 21:12:09 +0000

    Persist modal state

--- src/components/FlagonModal.tsx
+++ src/components/FlagonModal.tsx
@@ -1,23 +1,35 @@
 import React, { FC, useState } from 'react';
 
+import { useFlagon } from '../hooks/useFlagon';
 import { useKeyPress } from '../hooks/useKeyPress';
 import { options } from '../options';
 import styles from './FlagonModal.css';
 import { FlagonSettings } from './FlagonSettings';
 
+const MODAL_STATE = '_isModalOpen';
+
 interface Props {
   isDev: boolean;
   key?: string;
 }
+const persistModalState = ({ setShowLocal, setShowPersisted }) => (
+  show: boolean,
+) => {
+  setShowPersisted(show);
+  setShowLocal(show);
+};
 
 export const FlagonModal: FC<Props> = ({
   isDev,
   key = options.activationKey,
 }: Props) => {
-  const [show, setShow] = useState(true);
+  const { getValue, setValue } = useFlagon();
+  const showPersisted = getValue(MODAL_STATE);
+  const setShowPersisted = setValue(MODAL_STATE);
+  const [show, setShowLocal] = useState(showPersisted);
+  const setShow = persistModalState({ setShowLocal, setShowPersisted });
   const handleClose = (): void => setShow(false);
   const handleToggle = (): void => setShow(!show);
-
   useKeyPress(key, handleToggle);
 
   // Bail early if not in development
